c201a58c2b41d7ed2d77abe3ba3fd271019b87f0
Benson Imoh,ST <benson@stbensonimoh.com>
2021-06-27 13:58:33 +0100

    feat: Add SocialIcons components

---
+++ src/components/SocialIcons/social-icons.style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa"
+import theme from "../../theme.json"
+
+export const SocialIconsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+  font-size: 2rem;
+  color: ${theme.colors.bensonGrey};
+
+  & > a {
+    text-decoration: none;
+    color: ${theme.colors.bensonGrey};
+  }
+`
+
+export const GitHub = styled(FaGithub)``
+
+export const LinkedIn = styled(FaLinkedinIn)``
+
+export const Twitter = styled(FaTwitter)``
+
+export const Instagram = styled(FaInstagram)``
