e2f212190704827f6e85f86e2823ed370db6411c
Benson Imoh,ST <benson@stbensonimoh.com>
2021-06-27 11:47:33 +0100

    feat: Add HomePage Body

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Copyright } from '../components/HomePageBody/home-page.style'
 
 import Layout from "../components/layout"
+import HomePageBody from "../components/HomePageBody/HomePageBody"
 
 const App = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
   return (
     <Layout location={location} title={siteTitle}>
+          <HomePageBody />
+          <Copyright>Copyright &copy; { new Date().getFullYear() } Benson Imoh,ST</Copyright>
     </Layout>
   )
 }
