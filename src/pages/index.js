41ff96313dc3a96e1e8328f635c0d334df2ab45f
Benson Imoh,ST <benson@stbensonimoh.com>
2021-06-27 13:59:21 +0100

    feat: Add Social Icon components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Copyright } from '../components/HomePageBody/home-page.style'
+import { Copyright } from "../components/HomePageBody/home-page.style"
 
 import Layout from "../components/layout"
 import HomePageBody from "../components/HomePageBody/HomePageBody"
+import SocialIcons from "../components/SocialIcons/SocialIcons"
 
 const App = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
   return (
     <Layout location={location} title={siteTitle}>
-          <HomePageBody />
-          <Copyright>Copyright &copy; { new Date().getFullYear() } Benson Imoh,ST</Copyright>
+      <HomePageBody />
+      <SocialIcons />
+      <Copyright>
+        Copyright &copy; {new Date().getFullYear()} Benson Imoh,ST
+      </Copyright>
     </Layout>
   )
 }
