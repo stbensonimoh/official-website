ea0ba74904efc14dcc06ef1baef3e3650b402a51
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-28 15:54:44 +0100

    feat: Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+import Button from "../components/Button"
+import theme from "../theme.json"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,11 +13,43 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Container>
+        <img
+          src="/images/404.png"
+          width="40%"
+          alt="404 Image"
+          style={{ marginBottom: "1rem" }}
+        />
+        <Title>404</Title>
+        <p>
+          Awww... Are you lost? It seems like the page you are looking for does not
+          exist.
+        </p>
+        <Button to="/">Let's go home</Button>
+      </Container>
     </Layout>
   )
 }
+
+/* STYLES */
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & p {
+    font-family: "Roboto", "Open Sans", sans-serif;
+    margin-bottom: 2rem;
+  }
+`
+const Title = styled.h1`
+  font-family: "Dosis", sans-serif;
+  font-size: 500%;
+  font-weight: 300;
+  color: ${theme.colors.bensonPink};
+  margin-top: 0rem;
+  margin-bottom: 0rem;
+`
 
 export default NotFoundPage
 
