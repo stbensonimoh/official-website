06e5ef85945b944db108e3cd8f9b46a11f36759c
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-28 12:29:47 +0100

    refactor: Move Home Page styles in the component file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 import { graphql } from "gatsby"
-import {
-  Copyright,
-  CopyrightWrapper,
-  HomePageWrapper,
-} from "../components/HomePageBody/home-page.style"
 import Header from "../components/Header"
-import HomePageBody from "../components/HomePageBody/HomePageBody"
 import SocialIcons from "../components/SocialIcons"
+import theme from "../theme.json"
 
-const App = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-
+const App = () => {
   return (
-    <HomePageWrapper location={location} title={siteTitle}>
+    <HomePageWrapper>
       <Header />
-      <HomePageBody />
+      <BodyWrapper>
+        <FeaturedTextWrapper>
+          <GreetingWrapper>
+            <Greeting>Hello!</Greeting>
+            <Introduction>I am Benson...</Introduction>
+          </GreetingWrapper>
+          <Title>
+            Experience Designer (xD) <br />
+            Software Engineer <br />
+            Technology Advocate
+          </Title>
+          <Description black>
+            I'm passionate about blending technology and arts, to creatively and
+            efficiently solve problems, using new and experimental ideas and
+            methods. I build products with great User Experiences.
+          </Description>
+          <Description>
+            Highly skilled at design systems, automation, and customer
+            experience engineering.
+          </Description>
+        </FeaturedTextWrapper>
+        <FeaturedImageWrapper>
+          <FeaturedImage src="images/front-image.png" />
+        </FeaturedImageWrapper>
+      </BodyWrapper>
       <SocialIcons />
       <CopyrightWrapper>
         <Copyright>
@@ -26,14 +44,78 @@ const App = ({ data, location }) => {
   )
 }
 
-export default App
+/* STYLE */
+const HomePageWrapper = styled.div``
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
+const BodyWrapper = styled.div`
+  display: flex;
 `
+const FeaturedImageWrapper = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`
+
+const FeaturedImage = styled.img`
+  padding-left: 3rem;
+  width: 100%;
+  height: auto;
+  object-fit: contain;
+`
+
+const FeaturedTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin-top: 3rem;
+`
+
+const GreetingWrapper = styled.div`
+  margin-left: 3rem;
+`
+
+const Greeting = styled.h3`
+  font-family: "Roboto";
+  font-weight: 300;
+  font-size: 2.25rem;
+`
+
+const Introduction = styled.h4`
+  font-family: "Bad Script";
+  font-size: 1.5rem;
+  margin-top: -1rem;
+  font-weight: 300;
+`
+
+const Title = styled.h1`
+  margin-left: 3rem;
+  font-family: "Bebas Neue";
+  font-size: 3.5rem;
+  font-weight: 300;
+  letter-spacing: 0.1rem;
+  line-height: 1.1;
+`
+
+const Description = styled.p`
+  margin-left: 3rem;
+  font-family: "Roboto";
+  font-size: 0.875rem;
+  margin-top: -0.31rem;
+  width: 26rem;
+  color: ${({ black }) =>
+    black ? theme.colors.bensonBlack : theme.colors.bensonGrey};
+`
+
+const CopyrightWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const Copyright = styled.div`
+  margin-right: 3rem;
+  align-self: center;
+  font-family: "Roboto";
+  font-size: 14px;
+  color: ${theme.colors.bensonGrey};
+`
+
+export default App
