32a84e25a5fd692084cf1f2455a36f9fa2e63172
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-28 12:02:13 +0100

    fix: Fix issue with social media icons link spanning across the entire page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import {
 } from "../components/HomePageBody/home-page.style"
 import Header from "../components/Header"
 import HomePageBody from "../components/HomePageBody/HomePageBody"
-import SocialIcons from "../components/SocialIcons/SocialIcons"
+import SocialIcons from "../components/SocialIcons"
 
 const App = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
