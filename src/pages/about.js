2e6e2babca488bf47948add52e28708329ea3cd6
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-28 13:03:54 +0100

    refactor: Move the About Page and styles into a single component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,8 @@
 import React from "react"
-import Header from "../../components/Header"
-import {
-  AboutPageWrapper,
-  ContentWrapper,
-  FirstSectionWrapper,
-  HeaderImage,
-  IntroWrapper,
-  IntroTitle,
-  IntroText,
-  AboutSectionWrapper,
-  LeftContentWrapper,
-  RightContentWrapper,
-  ThirdSectionWrapper,
-  OtherSectionWrapper,
-  OtherSectionTitle,
-  OtherSectionText,
-  Button,
-} from "../../styles/about-page-style"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import theme from "../theme.json"
+import Header from "../components/Header"
 import { MdArrowForward } from "react-icons/md"
 
 const About = () => {
@@ -165,5 +151,160 @@ const About = () => {
     </AboutPageWrapper>
   )
 }
+
+
+/* ------------------------- STYLES ------------------------- */
+const AboutPageWrapper = styled.div``
+
+const FirstSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+const HeaderImage = styled.img`
+  margin-left: 9rem;
+  margin-top: 5rem;
+`
+
+const IntroWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 8rem;
+`
+
+const IntroTitle = styled.h1`
+  font-family: "Roboto", sans-serif;
+  font-weight: 700;
+  font-size: 4.375em;
+  margin-bottom: 1rem;
+
+  & > span {
+    color: ${theme.colors.bensonPink};
+  }
+`
+
+const IntroText = styled.p`
+  width: 55%;
+  font-family: "Roboto", sans-serif;
+  color: ${({ grey }) =>
+    grey ? theme.colors.bensonGrey : theme.colors.bensonBlack};
+  font-size: 1.125rem;
+  margin-top: ${({ grey }) => (grey ? "1rem" : "0.5rem")};
+`
+
+const AboutSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.colors.bensonPink};
+  height: 100vh;
+  color: #fff;
+`
+const LeftContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 8rem;
+  margin-top: 8rem;
+  width: 30%;
+
+  & > h1 {
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    font-size: 4rem;
+    width: 60%;
+  }
+
+  & > p {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.125rem;
+    line-height: 1.318rem;
+  }
+`
+
+const RightContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  margin-left: 8rem;
+  margin-top: 8rem;
+
+  & > p {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.125rem;
+    line-height: 1.318rem;
+  }
+
+  & > ul {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.125rem;
+    line-height: 1.318rem;
+  }
+`
+
+const ThirdSectionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+`
+const OtherSectionWrapper = styled.div`
+  width: 33.875rem;
+  height: 34.813rem;
+  background-color: #fff;
+  padding-left: 4.875rem;
+  padding-top: 5rem;
+  padding-right: 10.688rem;
+  padding-bottom: 7.6rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const OtherSectionTitle = styled.h1`
+  font-size: 3rem;
+  font-family: "Roboto", sans-serif;
+  font-weight: 900;
+  color: ${theme.colors.bensonPink};
+`
+
+const OtherSectionText = styled.p`
+  font-family: "Roboto", sans-serif;
+  font-size: 1.5rem;
+  color: ${theme.colors.bensonGrey};
+`
+
+const Button = styled(Link)`
+  padding: 1rem 3rem;
+  padding-right: 2rem;
+  width: 11rem;
+  background: #fff;
+  color: ${theme.colors.bensonPink};
+  text-transform: uppercase;
+  font-family: "Dosis";
+  text-decoration: none;
+  font-size: 1.5rem;
+  border: 1px solid ${theme.colors.bensonPink};
+  background: linear-gradient(
+      to right,
+      ${theme.colors.bensonPink} 50%,
+      #fff 50%
+    )
+    right;
+  background-size: 200% 100%;
+  background-position: right;
+  transition: all 0.7s ease-out;
+
+  &:hover {
+    background-position: left;
+    color: #fff;
+  }
+`
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
 
 export default About
