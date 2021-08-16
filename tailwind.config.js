2ef43efd308c05714f097b92cb1726d94e70d12f
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-16 19:09:57 +0100

    build: configure taildwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const colors = require("tailwindcss/colors")
+module.exports = {
+  purge: false,
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        bensonpink: {
+          DEFAULT: "#ec2c7c",
+        },
+        bensonblack: {
+          DEFAULT: "#000000",
+        },
+        bensongrey: {
+          DEFAULT: "#666666",
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+        bebas: ["Bebas Neue"],
+        badscript: ["Bad Script"],
+        dosis: ["Dosis"],
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  },
+}
