7e7036df15bda3ff1bd84752b9cbba1062c4fe03
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-17 12:32:24 +0100

    refactor: Make Social Icons use Tailwind css

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa"
 
-const SocialIcons = () => {
+const SocialIcons = (props) => {
   return (
-    <div>
+      <div {...props}>
       <a
         href="https://github.com/stbensonimoh"
         target="_blank"
