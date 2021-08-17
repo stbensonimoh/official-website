87ab2f705f662b3befb58da2949e90a715b82565
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-17 13:00:18 +0100

    refactor: Use Tailwind css on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,33 +7,31 @@ const App = () => {
   return (
     <div>
       <Header />
-      <div>
-        <div>
-          <div>
-            <h3>Hello!</h3>
-            <h4>I am Benson...</h4>
-          </div>
-          <h1>
+      <div className="flex items-center justify-between mt-12">
+        <div className="flex flex-col mx-10 w-1/3">
+            <h3 className="text-4xl font-roboto">Hello!</h3>
+            <h4 className="text-2xl font-badscript">I am Benson...</h4>
+                  <h1 className="font-bebas my-4" style={{fontSize: "3.125rem",lineHeight: "3rem"}}>
             Experience Designer (xD) <br />
             Software Engineer <br />
             Technology Advocate
           </h1>
-          <p>
+          <p className="w-4/5">
             I'm passionate about blending technology and arts, to creatively and
             efficiently solve problems, using new and experimental ideas and
             methods. I build products with great User Experiences.
           </p>
-          <p>
-            Highly skilled at design systems, automation, and customer
-            experience engineering.
+          <p className="pt-4 text-bensongrey w-3/5">
+            Highly skilled at design systems, automation, customer
+            experience, and developer experience engineering.
           </p>
         </div>
-        <div>
+        <div className="image-section mx-10">
           <img src="images/front-image.png" />
         </div>
       </div>
-      <SocialIcons />
-      <div>
+      <SocialIcons className="social-icons flex flex-col w-8 ml-4 text-center text-bensongrey" />
+      <div className="copyright-container float-right my-4 mx-8 font-roboto text-sm text-bensongrey">
         <Copyright />
       </div>
     </div>
