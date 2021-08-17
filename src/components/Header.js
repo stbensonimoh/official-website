09e4d0e9c4c6688f44376402338d98dca78827ed
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-17 11:40:31 +0100

    refactor: Use tailwind for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,35 +4,33 @@ import { Link } from "gatsby"
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <Link to="/">
-          <Logo className="" />
+    <div className="header flex justify-between items-center my-4">
+        <Link to="/" className="logo ml-4">
+          <Logo className="w-24" />
         </Link>
-      </div>
-      <nav>
+      <nav className="main-navigation uppercase text-base font-dosis mr-8">
         <Link to="/" activeClassName="active">
           Home
         </Link>
-        <Link to="/about" activeClassName="active">
+        <Link to="/about" activeClassName="active-menu-item">
           About
         </Link>
-        <Link to="/resume" activeClassName="active">
+        <Link to="/resume" activeClassName="active-menu-item">
           Resume
         </Link>
-        <Link to="/projects" activeClassName="active">
+        <Link to="/projects" activeClassName="active-menu-item">
           Projects
         </Link>
-        <Link to="/writing" activeClassName="active">
+        <Link to="/writing" activeClassName="active-menu-item">
           Writing
         </Link>
-        <Link to="/speaking" activeClassName="active">
+        <Link to="/speaking" activeClassName="active-menu-item">
           Speaking
         </Link>
-        <Link to="/blog" activeClassName="active">
+        <Link to="/blog" activeClassName="active-menu-item">
           Blog
         </Link>
-        <Link to="/contact" activeClassName="active">
+        <Link to="/contact" activeClassName="active-menu-item">
           Contact
         </Link>
       </nav>
