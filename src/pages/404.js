a36bddfe2b6080a043d28bc792506b0ecc9c3f27
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-28 22:07:29 +0100

    refactor: Make 404 page use Tailwind Css

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,19 +11,19 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <img
           src="/images/404.png"
           width="40%"
           alt="404 Image"
           style={{ marginBottom: "1rem" }}
         />
-        <h1>404</h1>
-        <p>
+        <h1 className="font-roboto font-bold text-bensonpink text-6xl">404</h1>
+        <p className="my-6">
           Awww... Are you lost? It seems like the page you are looking for does
           not exist.
         </p>
-        <Button to="/">Let's go home</Button>
+        <Button type="internal" to="/">Let's go home</Button>
       </div>
     </Layout>
   )
