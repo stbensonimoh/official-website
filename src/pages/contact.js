bfbeb1f7bbed8a94b430a858874db2451ec36c47
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-28 20:06:21 +0100

    refactor: Refactor Contact page to use tailwind.css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,19 +9,21 @@ const Contact = () => {
   return (
     <div className="h-screen">
       <Header />
-      <div className="main flex flex-col items-center">
-        <h1 className="font-roboto font-bold text-6xl text-bensonpink">Send me a message</h1>
+      <div className="main flex flex-col items-center justify-center mt-12">
+        <h1 className="font-roboto font-bold text-6xl text-bensonpink">
+          Send me a message
+        </h1>
         <p className="font-roboto text-2xl my-6 w-5/12 text-center">
           Do you have a question, a project you’d like to have me on, or just
           want to say hi?
         </p>
-        <form>
-          <div>
-            <div style={{ marginRight: "6rem" }}>
+        <form className="contact-form flex flex-col w-3/4 items-center mt-14">
+          <div className="row flex w-full">
+            <div className="column w-1/2 px-12">
               <label htmlFor="name">Your Name</label>
               <input type="text" placeholder="John Doe" id="name" />
             </div>
-            <div>
+            <div className="column w-1/2 px-12">
               <label htmlFor="email">Your Email</label>
               <input
                 type="email"
@@ -30,8 +32,8 @@ const Contact = () => {
               />
             </div>
           </div>
-          <div>
-            <div>
+          <div className="row my-14 w-full">
+            <div className="column w-full px-12">
               <label htmlFor="message">Your message</label>
               <textarea
                 rows="2"
@@ -41,16 +43,18 @@ const Contact = () => {
             </div>
           </div>
           <div>
-            <Button>Send <FiSend /></Button>
+            <Button>
+              Send &nbsp; <FiSend />
+            </Button>
           </div>
         </form>
-        <div>
-          <SocialIcons />
-        </div>
-        <div>
-          <Copyright />
-        </div>
       </div>
+        <SocialIcons className="absolute bottom-10 social-icons flex flex-col w-8 ml-4 text-center text-bensongrey" />
+        <div>
+          <div className="copyright-container absolute right-8 bottom-8 font-roboto text-sm text-bensongrey">
+            <Copyright />
+          </div>
+        </div>
     </div>
   )
 }
