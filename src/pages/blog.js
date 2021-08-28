309a0287842b5499513d20b87408442c7f7f5340
Benson Imoh,ST <benson@stbensonimoh.com>
2021-08-28 21:57:47 +0100

    refactor: Refactor Blog page to use Tailwind CSS

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,45 @@
 import React from "react"
+import Button from "../components/Button"
 import Header from "../components/Header"
 
 const Blog = () => {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col">
+      <div className="first-section flex flex-col h-screen">
         <Header />
-        <section>
+        <section
+          className="flex justify-center items-center flex-grow space-x-12"
+          style={{ backgroundColor: "#f9f9f9" }}
+        >
           <div>
-            <h2>Welcome to my blog</h2>
-            <h1>We all owe death a life.</h1>
-            <p>
+            <h2 className="text-lg font-dosis uppercase text-bensonpink">
+              Welcome to my blog
+            </h2>
+            <h1 className="text-5xl font-roboto font-bold w-2/4 my-6">
+              We all owe death a life.
+            </h1>
+            <p className="font-roboto w-2/4 mb-4">
               I write about technology, design, engineering, productivity hacks,
               and life generally...
             </p>
-            <div>
-              <input type="text" placeholder="Your name" />
-              <input type="email" placeholder="Your email" />
-              <button>Subscribe</button>
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Your name"
+                className="bg-transparent border text-roboto px-4 mr-4 outline-none"
+              />
+              <input
+                type="email"
+                placeholder="Your email"
+                className="bg-transparent border text-roboto px-4 mr-4 outline-none"
+              />
+              <Button>Subscribe</Button>
             </div>
           </div>
           <img src="/images/blog-header-image.png" />
         </section>
-      </div>
+          </div>
+          <div className="second-section"></div>
     </div>
   )
 }
