7bd36185f2797efef2d96c3502f217f37a7094ae
Benson Imoh,ST <benson@stbensonimoh.com>
2023-08-21 23:23:17 +0100

    refactor: :recycle: Refactor BlogPostCard component
    cleanup the `BlogPostCard` component

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -1,51 +1,50 @@
 import React from "react"
-import AuthorBlob from "./AuthorBlob"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import AuthorBlob from "../components/AuthorBlob"
 
 const BlogPostCard = ({ post }) => {
-  const data = useStaticQuery(graphql`
-    query BlogPostCardQuery {
-      site {
-        siteMetadata {
-          author {
-            name
-            image
-          }
-        }
-      }
-    }
-  `)
+  const { author } = useStaticQuery(BLOG_POST_CARD_QUERY).site.siteMetadata
 
-  console.log(data)
   return (
     <div className="bg-white w-full p-8 rounded-md shadow-sm">
-      <div className="image-container max-h-52 overflow-hidden">
-        <img src={post.frontmatter.featured_image} />
-      </div>
+      <Link to={post.fields.slug}>
+        <div className="image-container max-h-52 overflow-hidden">
+          <img src={post.frontmatter.featured_image} alt="Featured Image" />
+        </div>
+      </Link>
       <h2 className="text-lg font-roboto font-bold text-bensonblack my-2">
-        {post.frontmatter.title}
+        <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
       </h2>
       <AuthorBlob
-        author={
-          post.frontmatter?.author
-            ? post.frontmatter.author
-            : data.site.siteMetadata.author.name
-        }
+        author={post.frontmatter?.author ?? author.name}
         date={new Date(post.frontmatter.date).toLocaleDateString("en-US", {
           year: "numeric",
           month: "long",
           day: "numeric",
         })}
         timeToRead={post.fields.timeToRead.text}
-        image={
-          post.frontmatter?.author_image
-            ? post.frontmatter.author_image
-            : data.site.siteMetadata.author.image
-        }
+        image={post.frontmatter?.author_image ?? author.image}
       />
-      <p className="font-slabt">{post.excerpt}</p>
+      <p>{post.excerpt}</p>
+
+      <span className="text-bensonpink">
+        <Link to={post.fields.slug}>Read More...</Link>
+      </span>
     </div>
   )
 }
 
 export default BlogPostCard
+
+export const BLOG_POST_CARD_QUERY = graphql`
+  query BlogPostCardQuery {
+    site {
+      siteMetadata {
+        author {
+          name
+          image
+        }
+      }
+    }
+  }
+`
