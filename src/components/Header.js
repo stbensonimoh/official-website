b8f4d3b7c291cb99980a308938c9e4a8b32382bf
Benson Imoh,ST <benson@stbensonimoh.com>
2023-10-29 13:15:38 +0100

    fix: Fix navbar responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import Logo from "./Logo"
 import { Link } from "gatsby"
+import { FiX, FiMenu } from "react-icons/fi"
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
   const menuItems = [
     {
       name: "Home",
@@ -46,29 +52,66 @@ const Header = () => {
     },
   ]
   return (
-    <div className="header flex justify-between items-center my-4">
-      <Link to="/" className="logo ml-4">
-        <Logo className="w-24" />
-      </Link>
-      <nav className="main-navigation uppercase text-base font-dosis mr-8">
-        {menuItems.map((item, index) =>
-          item.internal ? (
-            <Link
-              key={index}
-              to={item.link}
-              className="nav-item"
-              activeClassName="active-menu-item"
-            >
-              {item.name}
-            </Link>
-          ) : (
-            <a key={index} href={item.link} className="nav-item">
-              {item.name}
-            </a>
-          )
-        )}
-      </nav>
-    </div>
+    <>
+      <div className="hidden header md:flex justify-between items-center my-4">
+        <Link to="/" className="logo ml-4">
+          <Logo className="w-24" />
+        </Link>
+        <nav className="main-navigation uppercase text-base font-dosis mr-8">
+          {menuItems.map((item, index) =>
+            item.internal ? (
+              <Link
+                key={index}
+                to={item.link}
+                className="nav-item"
+                activeClassName="active-menu-item"
+              >
+                {item.name}
+              </Link>
+            ) : (
+              <a key={index} href={item.link} className="nav-item">
+                {item.name}
+              </a>
+            )
+          )}
+        </nav>
+      </div>
+
+      <div
+        className={`${
+          menuOpen ? "-translate-y-0" : "-translate-y-full"
+        } absolute w-full flex flex-col items-center h-screen bg-white z-[20] md:hidden transition transform ease-in-out duration-700`}
+      >
+        <Link to="/" className="logo">
+          <Logo className="w-24" />
+        </Link>
+        <nav className="uppercase text-2xl font-dosis flex flex-col items-center h-1/2 mt-24 justify-between">
+          {menuItems.map((item, index) =>
+            item.internal ? (
+              <Link
+                key={index}
+                to={item.link}
+                className="nav-item"
+                activeClassName="active-menu-item"
+              >
+                {item.name}
+              </Link>
+            ) : (
+              <a key={index} href={item.link} className="nav-item">
+                {item.name}
+              </a>
+            )
+          )}
+        </nav>
+      </div>
+
+      <button
+        className="absolute top-8 right-8 text-3xl z-[21] md:hidden"
+        onClick={toggleMenu}
+      >
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </button>
+    </>
   )
 }
 
