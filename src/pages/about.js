43cfef0363d5fc599ca9a2116d33a5365f429a71
Benson Imoh,ST <benson@stbensonimoh.com>
2023-12-09 23:16:51 +0100

    Fix: Fix the mobile responsiveness of the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,35 +7,38 @@ import { HeadSeo } from "gatsby-plugin-head-seo/src"
 const About = () => {
   return (
     <div className="flex flex-col">
-      <div className="lg:h-screen">
+      <div className="w-full lg:h-screen">
         <Header />
-        <div className="flex items-center justify-center">
-          <img src="/images/about-page-picture.png" className="ml-16 my-20" />
-          <div className="mx-16 w-2/5">
-            <h1 className="lg:text-4xl xl:text-5xl font-roboto font-bold leading-tight">
+        <div className="flex flex-col pt-20 md:pt-0 pb-12 md:pb-0 md:flex-row items-center justify-center">
+          <img
+            src="/images/about-page-picture.png"
+            className="px-10 md:ml-16 mt-20 mb-9 md:mt-0"
+          />
+          <div className="mx-10 md:mx-16 md:w-2/5 text-center md:text-left">
+            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-roboto font-bold leading-tight">
               Experience Designer<span className="text-bensonpink">.</span>
               <br />
               Software Engineer<span className="text-bensonpink">.</span>
               <br />
               OSS Advocate<span className="text-bensonpink">.</span>
             </h1>
-            <p className="my-4">
+            <p className="hidden md:flex my-4">
               I always come up with interesting ways to blend engineering and
               design in efficiently solving everyday problems for individuals
               and businesses that I work with; seeking smarter and newer ways to
               do old things while saving time and resources at the same time.
               I'm enthusiastic about OSS.
             </p>
-            <p className="text-bensongrey">
+            <p className="hidden md:flex text-bensongrey">
               Highly skilled in Experience Design(xD), Customer Experience(Cx),
               automation, and Developer Experience(Dx) Engineering.
             </p>
           </div>
         </div>
       </div>
-      <div className="flex px-12 py-20 items-center bg-bensonpink h-screen text-white font-roboto">
-        <div className="mx-10">
-          <h1 className="text-6xl font-bold w-2/3 py-8">
+      <div className="flex flex-col w-full md:flex-row px-12 py-20 items-center bg-bensonpink md:h-screen text-white font-roboto">
+        <div className="md:mx-10">
+          <h1 className="text-6xl font-bold md:w-2/3 py-8 md:py-8">
             Over the past years,
           </h1>
           <p className="my-4">
@@ -74,7 +77,7 @@ const About = () => {
             communities in Zambia.
           </p>
         </div>
-        <div className="mx-10">
+        <div className="md:mx-10 mt-8 md:mt-0">
           <p>At AWLO I:</p>
           <ul className="list-disc ml-8">
             <li>
@@ -126,14 +129,14 @@ const About = () => {
         </div>
       </div>
       <div
-        className="h-screen flex items-center justify-center"
+        className="flex flex-col md:flex-row items-center justify-center md:h-screen"
         style={{ backgroundColor: "#f9f9f9" }}
       >
-        <div className="bg-white px-20 pt-20 pb-32 w-1/3">
-          <h1 className="text-5xl font-roboto font-bold text-bensonpink w-11/12">
+        <div className="bg-white px-10 md:px-20 pt-12 pb-20 md:pt-20 md:pb-32 w-10/12 md:w-1/3">
+          <h1 className="text-4xl md:text-5xl font-roboto font-bold text-bensonpink w-11/12">
             I build and design stuff
           </h1>
-          <p className="text-2xl my-10 text-bensongrey w-11/12">
+          <p className="md:text-2xl my-10 text-bensongrey w-11/12">
             Web Apps, freelance projects, personal projects and experiments
           </p>
           <Button type="internal" to="/projects">
@@ -141,11 +144,11 @@ const About = () => {
             <MdArrowForward />
           </Button>
         </div>
-        <div className="bg-white px-20 pt-20 pb-32 w-1/3">
-          <h1 className="text-5xl font-roboto font-bold text-bensonpink w-11/12">
+        <div className="flex  flex-col bg-white px-10 md:px-20 pt-12 pb-20 my-20 mt-10 md:mt-20 md:pt-20 md:pb-32 w-10/12 md:w-1/3">
+          <h1 className="text-4xl md:text-5xl font-roboto font-bold text-bensonpink w-11/12">
             I write too, sometimes
           </h1>
-          <p className="text-2xl my-10 text-bensongrey w-11/12">
+          <p className="md:text-2xl my-8 md:my-10 text-bensongrey w-11/12">
             About OSS, technology, design, engineering, productivity and
             business
           </p>
