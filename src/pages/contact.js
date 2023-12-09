6057307051b8d21a96d5917f36c095b2096af5ed
Benson Imoh,ST <benson@stbensonimoh.com>
2023-12-09 23:49:51 +0100

    Fix: Fix mobile responsiveness on mobile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -70,62 +70,74 @@ const Contact = () => {
     }
   }
   return (
-    <div className="h-screen">
+    <>
       <Header />
-      <div className="main flex flex-col items-center justify-center mt-12">
-        <h1 className="font-roboto font-bold text-6xl text-bensonpink">
-          Send me a message
-        </h1>
-        <p className="font-roboto text-2xl my-6 w-5/12 text-center">
-          Do you have a question, a project you’d like to have me on, or just
-          want to say hi?
+      <div className="flex flex-col pt-20 w-full justify-center items-center md:hidden">
+        <img src="images/contact-vector.png" className="my-20 mb-8" />
+        <p className="text-center font-roboto mx-10 mb-8">
+          Got a question or proposal, or just want to say hello? Go ahead.
         </p>
-        <form className="contact-form flex flex-col w-3/4 items-center mt-14">
-          <div className="row flex w-full">
-            <div className="column w-1/2 px-12">
-              <label htmlFor="name">Your Name</label>
-              <input
-                type="text"
-                placeholder="John Doe"
-                id="name"
-                onChange={handleContactName}
-              />
-            </div>
-            <div className="column w-1/2 px-12">
-              <label htmlFor="email">Your Email</label>
-              <input
-                type="email"
-                placeholder="johndoe@example.com"
-                onChange={handleContactEmail}
-                id="email"
-              />
-            </div>
-          </div>
-          <div className="row my-14 w-full">
-            <div className="column w-full px-12">
-              <label htmlFor="message">Your message</label>
-              <textarea
-                rows="2"
-                onChange={handleContactMessage}
-                placeholder="Hi, I think we need a design system for our products at Company X. How soon can you hop on to discuss this?"
-                id="message"
-              ></textarea>
-            </div>
-          </div>
-          <div>
-            <Button onClick={handleSendEmail}>
-              Send &nbsp; <FiSend />
-            </Button>
-          </div>
-        </form>
+        <SocialIcons className="mb-4 social-icons flex flex-row text-center text-bensonpink" />
+        <Button type="external" href="mailto:benson@stbensonimoh.com">
+          Send me a mail
+        </Button>
       </div>
-      <SocialIcons className="absolute bottom-10 social-icons flex flex-col w-8 ml-4 text-center text-bensongrey" />
-      <div>
-        <div className="copyright-container absolute right-8 bottom-8 font-roboto text-sm text-bensongrey">
-          <Copyright />
+      <div className="hidden md:flex flex-col h-screen">
+        <div className="main flex flex-col items-center justify-center mt-12">
+          <h1 className="font-roboto font-bold text-6xl text-bensonpink">
+            Send me a message
+          </h1>
+          <p className="font-roboto text-2xl my-6 w-5/12 text-center">
+            Do you have a question, a project you’d like to have me on, or just
+            want to say hi?
+          </p>
+          <form className="contact-form flex flex-col w-3/4 items-center mt-14">
+            <div className="row flex w-full">
+              <div className="column w-1/2 px-12">
+                <label htmlFor="name">Your Name</label>
+                <input
+                  type="text"
+                  placeholder="John Doe"
+                  id="name"
+                  onChange={handleContactName}
+                />
+              </div>
+              <div className="column w-1/2 px-12">
+                <label htmlFor="email">Your Email</label>
+                <input
+                  type="email"
+                  placeholder="johndoe@example.com"
+                  onChange={handleContactEmail}
+                  id="email"
+                />
+              </div>
+            </div>
+            <div className="row my-14 w-full">
+              <div className="column w-full px-12">
+                <label htmlFor="message">Your message</label>
+                <textarea
+                  rows="2"
+                  onChange={handleContactMessage}
+                  placeholder="Hi, I think we need a design system for our products at Company X. How soon can you hop on to discuss this?"
+                  id="message"
+                ></textarea>
+              </div>
+            </div>
+            <div>
+              <Button onClick={handleSendEmail}>
+                Send &nbsp; <FiSend />
+              </Button>
+            </div>
+          </form>
+        </div>
+        <SocialIcons className="absolute bottom-10 social-icons flex flex-col w-8 ml-4 text-center text-bensongrey" />
+        <div>
+          <div className="copyright-container absolute right-8 bottom-8 font-roboto text-sm text-bensongrey">
+            <Copyright />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
