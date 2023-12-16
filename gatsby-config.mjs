efb900332137e15871cfcf600ff956b4ca6b45fe
Benson Imoh,ST <benson@stbensonimoh.com>
2023-12-16 07:55:54 +0100

    feat: :chart_with_upwards_trend: Add Google Analytics to the project

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -56,6 +56,18 @@ const config = {
         path: `${__dirname}/content/blog`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [`G-4C79DP5RD8`],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
 }
 
