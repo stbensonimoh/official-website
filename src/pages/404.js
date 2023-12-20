a1802e98d4f4a401338cde25b0d63020a0d7809e
Benson Imoh,ST <benson@stbensonimoh.com>
2023-12-20 23:45:53 +0100

    fix: :adhesive_bandage: Fix responsiveness for the 404 page on all devices.
    Make the 404 page responsive on screen of all sizes.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,42 +1,33 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Button from "../components/Button"
+import Header from "../components/Header"
 
-import Layout from "../components/layout"
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const NotFoundPage = () => {
   return (
-    <Layout location={location} title={siteTitle}>
-      <div className="flex flex-col items-center justify-center font-roboto">
-        <img
-          src="/images/404.png"
-          width="40%"
-          alt="404 Image"
-          style={{ marginBottom: "1rem" }}
-        />
-        <h1 className="font-roboto font-bold text-bensonpink text-6xl">404</h1>
-        <p className="my-6">
-          Awww... Are you lost? It seems like the page you are looking for does
-          not exist.
-        </p>
-        <Button type="internal" to="/">
-          Let's go home
-        </Button>
-      </div>
-    </Layout>
+    <>
+      <Header />
+      <main>
+        <div className="flex flex-col items-center justify-center font-roboto pt-60 md:pt-20 mx-10">
+          <img
+            src="/images/404.png"
+            width="40%"
+            alt="404 Image"
+            style={{ marginBottom: "1rem" }}
+          />
+          <h1 className="font-roboto font-bold text-bensonpink text-6xl">
+            404
+          </h1>
+          <p className="my-6 text-center">
+            Awww... Are you lost? It seems like the page you are looking for
+            does not exist.
+          </p>
+          <Button type="internal" to="/">
+            Let's go home
+          </Button>
+        </div>
+      </main>
+    </>
   )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
