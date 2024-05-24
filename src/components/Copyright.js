2371e03af65df52088b8d6e944c357dbf5c0aca3
Benson Imoh,ST <benson@stbensonimoh.com>
2024-05-24 22:15:00 +0100

    feat: Port the project over to Next.js
    - Delete all the project files
    - Install Next.js 15-rc
    - Install React 19-rc

--- src/components/Copyright.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Copyright = props => {
-  return (
-    <p {...props}>Copyright &copy; {new Date().getFullYear()} Benson Imoh,ST</p>
-  )
-}
-
-export default Copyright
