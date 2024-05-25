1de6ab8e0bd828d01e9b8b591b98664acb7679b4
Benson Imoh,ST <benson@stbensonimoh.com>
2024-05-25 16:52:04 +0100

    feat: Add the Header component with navigation to the default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { bebas, roboto, dosis, badscript, slab } from "./fonts";
+import Header from "@/app/components/Header";
 
 export default function RootLayout({
   children,
@@ -12,6 +13,7 @@ export default function RootLayout({
       <body
         className={`${bebas.variable} ${roboto.variable} ${badscript.variable} ${dosis.variable} ${slab.variable}`}
       >
+        <Header />
         {children}
       </body>
     </html>
