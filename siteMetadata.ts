4a18d87bab47401d354e1b5937e57009bdcf8cb7
Benson Imoh,ST <benson@stbensonimoh.com>
2024-05-25 21:41:43 +0100

    feat: Port siteMetadaaa to Next.js

---
+++ siteMetadata.ts
@@ -0,0 +1,20 @@
+const siteMetadata = {
+  title: `Benson Imoh,ST`,
+  author: {
+    name: `Benson Imoh,ST`,
+    summary: `Software Engineer | Experience Designer(xD) | OSS Enthusiast. Passionate about blending engineering and design, to creatively and efficiently solve problems.`,
+    image: `https://res.cloudinary.com/stbensonimoh/image/upload/v1692398633/sq_xmnmhb.jpg`,
+  },
+  description: `Software Engineer | Experience Designer(xD) | OSS Enthusiast`,
+  siteUrl: `https://stbensonimoh.com/`,
+  social: {
+    twitter: `stbensonimoh`,
+    linkedin: `stbensonimoh`,
+    github: `stbensonimoh`,
+    instagram: `stbensonimoh`,
+    facebook: `stbensonimoh`,
+    youtube: `stbensonimoh`,
+  },
+};
+
+export default siteMetadata;
