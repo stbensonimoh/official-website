74bda9fdac0a0138f7734b50751217397dc4afb2
Benson Imoh,ST <benson@stbensonimoh.com>
2024-07-22 16:43:31 +0100

    Add the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-
+import Copyright from "@/app/components/Copyright";
+import Button from "@/app/components/Button";
+import SocialIcons from "@/app/components/SocialIcons";
 export default function Home() {
-  return <div className="font-roboto">Hello World</div>;
+  return (
+    <div className="font-roboto">
+      Hello World
+      <Copyright />
+      <Button type="button">Submit</Button>
+      <SocialIcons className="absolute bottom-10 social-icons hidden md:flex flex-col w-8 ml-4 text-center text-bensongrey" />
+    </div>
+  );
 }
