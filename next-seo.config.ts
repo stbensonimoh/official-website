d0ab0af77d56c30d1a8a7547461734d7518d4941
Benson Imoh, ST <benson@stbensonimoh.com>
2024-12-26 20:48:30 +0100

    feat: Add default SEO metadata

---
+++ next-seo.config.ts
@@ -0,0 +1,17 @@
+const defaultSEOConfig = {
+  title: "Benson Imoh,ST",
+  description: "Software Engineer. DevOps Enthusiast. OSS Advocate.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://stbensonimoh.com/",
+    site_name: "Benson Imoh,ST",
+  },
+  twitter: {
+    handle: "@stbensonimoh",
+    site: "@stbensonimoh",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
